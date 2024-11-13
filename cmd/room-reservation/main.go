c1805ae45bbe27a749545ca251a326593747f014
Kaiser Sakhi <mail@kaisersakhi.com>
2024-11-13 11:51:30 +0530

    Add air live reloading

--- cmd/room-reservation/main.go
+++ cmd/room-reservation/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main(){
-	fmt.Println("Hello world.")
+	port := flag.String("port", "8080", "port to run the application on")
+	env := flag.String("env", "production", "application environment (development, production)")
+
+	flag.Parse()
+
+	fmt.Printf("Running on port: %s\n", *port)
+	fmt.Printf("Environment: %s\n", *env)
+	fmt.Println("d")
 }
